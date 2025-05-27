e8e252c5a224889e2d6adc527a66b36968d60fd8
hnkh <stiupfay@gmail.com>
2025-05-27 21:50:08 +0330

    shit

--- src/routes/work/[slug]/index.tsx
+++ src/routes/work/[slug]/index.tsx
@@ -1,13 +1,15 @@
 import { component$ } from '@builder.io/qwik';
 import { useLocation } from '@builder.io/qwik-city';
 import { Header } from '~/components/Header';
+import { CategoryLinks } from '~/components/Work/CategoryLinks';
 import { Works } from '~/components/Work/Works';
 
 export default component$(() => {
   const location=useLocation();
   return (
-    <div class="">
+    <div>
       <Header class="relative z-20"/>
+      <CategoryLinks/>
       <Works category={location.params.slug} />
     </div>
   );
--- src/routes/work/index.tsx
+++ src/routes/work/index.tsx
@@ -1,43 +1,14 @@
-import { component$, Resource, useResource$ } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { Header } from "~/components/Header";
-import { CategoryLink } from "~/components/Work/CategoryLink";
+import { CategoryLinks } from "~/components/Work/CategoryLinks";
 import { Works } from "~/components/Work/Works";
 
 export default component$(() => {
-  const categoriesResource = useResource$<any[]>(async () => {
-    const response = await fetch("http://localhost:3000/api/categories");
-    const data = await response.json();
-
-    return data.docs.map((category: any) => ({
-      id: category.id,
-      label: category.title,
-      slug: category.slug,
-    }));
-  });
-
-
   return (
     <div>
       <Header class="relative z-20" />
-      <div class="mb-20 flex w-full flex-wrap text-[clamp(1rem,1.5vw,1.5rem)] md:w-2/3">
-        <Resource
-          value={categoriesResource}
-          onPending={() => <div>Loading...</div>}
-          onRejected={() => <div>Error loading data</div>}
-          onResolved={(categories) =>
-            categories.map((category) => (
-              // <a key={category.id} href={"/category/" + category.slug}> {category.label}</a>
-              <CategoryLink
-                key={category.id}
-                slug={category.slug}
-                label={category.label}
-                isActive={false}
-                class="mt-6 w-[clamp(10rem,15vw,15rem)]"
-              />
-            ))
-          }
-        />
-      </div>
+      <CategoryLinks />
+
       <Works />
     </div>
   );
